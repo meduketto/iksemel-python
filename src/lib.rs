7b70c9442e0cf0f2da1b4a11ee8185eea17264fa
Gurer Ozen <meduketto@gmail.com>
2025-11-04 20:00:19 -0500

    exception mapping

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,46 @@
+/*
+** This file is a part of Iksemel (XML parser for Jabber/XMPP)
+** Copyright (C) 2000-2025 Gurer Ozen
+**
+** Iksemel is free software: you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as
+** published by the Free Software Foundation, either version 3 of
+** the License, or (at your option) any later version.
+*/
+
+use pyo3::create_exception;
+use pyo3::exceptions::PyException;
+use pyo3::exceptions::PyMemoryError;
 use pyo3::prelude::*;
 
 use iks::DocumentParser;
+use iks::ParseError;
 use iks::SyncCursor;
+
+create_exception!(pyiks, BadXmlError, PyException);
+
+enum PyIksError {
+    NoMemory,
+    BadXml(&'static str),
+}
+
+impl From<ParseError> for PyIksError {
+    fn from(err: ParseError) -> Self {
+        match err {
+            ParseError::NoMemory => PyIksError::NoMemory,
+            ParseError::BadXml(msg) => PyIksError::BadXml(msg),
+        }
+    }
+}
+
+impl From<PyIksError> for PyErr {
+    fn from(err: PyIksError) -> Self {
+        match err {
+            PyIksError::NoMemory => PyMemoryError::new_err("iks alloc failed"),
+            PyIksError::BadXml(msg) => BadXmlError::new_err(msg),
+        }
+    }
+}
 
 #[pyclass(name = "Document", frozen)]
 struct PyDocument {
@@ -11,12 +50,25 @@ struct PyDocument {
 #[pymethods]
 impl PyDocument {
     #[new]
-    fn new(bytes: &[u8]) -> Self {
+    fn new(bytes: &[u8]) -> Result<Self, PyIksError> {
         let mut parser = DocumentParser::with_size_hint(bytes.len());
-        parser.parse_bytes(bytes).unwrap();
-        let document = parser.into_document().unwrap();
+        parser.parse_bytes(bytes)?;
+        let document = parser.into_document()?;
         let inner = SyncCursor::new(document);
-        Self { inner }
+        Ok(Self { inner })
+    }
+
+    fn find_tag(&self, tag: &str) -> Option<Self> {
+        let node = self.inner.clone().find_tag(tag);
+        if node.is_null() {
+            Some(Self { inner: node })
+        } else {
+            None
+        }
+    }
+
+    fn __str__(&self) -> String {
+        self.inner.clone().to_string()
     }
 }
 
